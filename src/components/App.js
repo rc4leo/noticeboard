b058f33621ed4cda8199dabdb67923673c488b79
Daryl Chan <dckj95@gmail.com>
2018-09-07 00:34:34 +0800

    Added header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/App.css';
 
+import Header from './Header';
+
 class App extends React.Component {
   render() {
     return (
       <div className="app">
-        noticeboard
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        RC4 LEO
+      </header>
+    );
+  }
+}
+
+export default Header;
