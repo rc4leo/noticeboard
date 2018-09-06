86c31a3fb3b3f902a69edcae3d03bb93210ccefc
Daryl Chan <dckj95@gmail.com>
2018-09-07 00:11:32 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// External imports
+import React, { Component } from 'react';
+
+// Internal imports
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        noticeboard
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+// External imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Internal imports
+import './index.css';
+import App from './App';
+
+// Render
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// Service worker (inactive)
+// import registerServiceWorker from './registerServiceWorker';
+// registerServiceWorker();
